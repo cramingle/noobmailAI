16e556e7a8a715e00c2c3d47396bd527be1d1452
cramingle <cramingle@gmail.com>
2025-02-26 04:28:53 +0700

    Optimize Vercel and SvelteKit configuration for deployment

--- frontend/src/routes/+page.ts
+++ frontend/src/routes/+page.ts
@@ -1,6 +1,9 @@
 import { redirect } from '@sveltejs/kit';
 import type { PageLoad } from './$types';
 
+export const prerender = false;
+export const ssr = true;
+
 export const load: PageLoad = async () => {
     throw redirect(307, '/write');
 }; 
