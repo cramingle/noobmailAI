1c24bb5fb1a886e4a1105976b01994676ae9ba8c
cramingle <cramingle@gmail.com>
2025-02-26 04:20:26 +0700

    Fix routing configuration to handle 404s

--- frontend/src/routes/+page.ts
+++ frontend/src/routes/+page.ts
@@ -1,6 +1,12 @@
 import { redirect } from '@sveltejs/kit';
 import type { PageLoad } from './$types';
 
-export const load: PageLoad = async () => {
+export const load: PageLoad = async ({ url }) => {
+    // If we're already on /write, don't redirect
+    if (url.pathname === '/write') {
+        return {};
+    }
+    
+    // Otherwise redirect to /write
     throw redirect(307, '/write');
 }; 
